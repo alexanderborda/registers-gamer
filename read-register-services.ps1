e4be35cd29a3e113c4a0f1b90d076757be05667a
Alexander Borda <10381413+alexanderborda@users.noreply.github.com>
2024-06-21 17:35:23 -0400

    Update read-register-services.ps1
    Se adicionaron un contador de las rutas no encontradas en el registro y una lista de las rutas no encontradas

--- read-register-services.ps1
+++ read-register-services.ps1
@@ -1,5 +1,8 @@
+# Obtener la fecha y hora actual
+$now = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+
 # Ruta del archivo de log
-$logFilePath = "C:\RegeditLogFile.txt"
+$logFilePath = "C:\Register_Logs_$now.txt"
 
 # Check if the script is running with admin rights
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -14,75 +17,155 @@ if (-not (Test-Path $logFilePath)) {
     New-Item -Path $logFilePath -ItemType File | Out-Null
 }
 
-# Función para mostrar el valor del registro y registrar la salida
+# Inicializar contador de registros y keys no encontrados
+$keypathsNoFound = 0
+$keysNoFound = 0
+$missingKeyPaths = @()
+
+# Función para mostrar el valor actual de las claves leidas.
 function Show-RegistryValue {
     param (
         [string]$keyPath,
         [string]$valueName,
         [string]$logFilePath
     )
-
     try {
         if (Test-Path $keyPath) {
             $value = Get-ItemProperty -Path $keyPath -Name $valueName -ErrorAction Stop
             if ($null -ne $value) {
-                $output = "Service: $keyPath, $valueName $($value.$valueName)"
-            } else {
-                $output = "Service: $keyPath, $valueName Not found"
+                $output = "KeyPath: $keyPath, $valueName $($value.$valueName)"
             }
-        } else {
-            $output = "Service: $keyPath, Path: Not found"
+            else {
+                $output = "Key: $valueName Key Not found"
+                $global:keysNoFound++
+            }
         }
-    } catch {
-        $output = "Error accessing $keyPath $_"
+        else {
+            $output = "KeyPath: $keyPath, keyPath Not found"
+            # Agregar keyPath no encontrado al arreglo
+            $global:missingKeyPaths += $keyPath
+            # Incrementar contador de rutas no encontradas
+            $global:keypathsNoFound++
+        }
     }
-
+    catch {
+        $output = "Error accessing $keyPath $_"
+        # Agregar keyPath no encontrado al arreglo
+        $global:missingKeyPaths += $keyPath
+        # Incrementar contador de rutas no encontradas
+        $global:keypathsNoFound++
+    }
     Write-Host $output
     Add-Content -Path $logFilePath -Value $output
+    
 }
-
-# Lista de servicios y sus valores de registro
-$services = @(
-    @{Name = "BITS"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\BITS"},
-    @{Name = "BTAGService"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\BTAGService"},
-    @{Name = "bthserv"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\bthserv"},
-    @{Name = "lfsvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc"},
-    @{Name = "DiagTrack"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\DiagTrack"},
-    @{Name = "HvHost"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\HvHost"},
-    @{Name = "vmickvpexchange"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmickvpexchange"},
-    @{Name = "vmicguestinterface"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicguestinterface"},
-    @{Name = "vmicshutdown"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicshutdown"},
-    @{Name = "vmicheartbeat"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicheartbeat"},
-    @{Name = "vmicvmsession"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicvmsession"},
-    @{Name = "vmicrdv"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicrdv"},
-    @{Name = "vmictimesync"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmictimesync"},
-    @{Name = "vmicvss"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicvss"},
-    @{Name = "PhoneSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PhoneSvc"},
-    @{Name = "Spooler"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Spooler"},
-    @{Name = "QWAVE"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\QWAVE"},
-    @{Name = "SysMain"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\SysMain"},
-    @{Name = "WSearch"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\WSearch"},
-    @{Name = "UserDataSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\UserDataSvc"},
-    @{Name = "UnistoreSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\UnistoreSvc"},
-    @{Name = "UdkUserSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\UdkUserSvc"},
-    @{Name = "PimIndexMaintenanceSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PimIndexMaintenanceSvc"},
-    @{Name = "DevicesFlowUserSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\DevicesFlowUserSvc"},
-    @{Name = "PrintWorkflowUserSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PrintWorkflowUserSvc"},
-    @{Name = "PenService"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PenService"},
-    @{Name = "P9RdrService"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\P9RdrService"},
-    @{Name = "MessagingService"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\MessagingService"},
-    @{Name = "BluetoothUserService"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\BluetoothUserService"},
-    @{Name = "webthreatdefusersvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\webthreatdefusersvc"},
-    @{Name = "NPSMSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\NPSMSvc"},
-    @{Name = "OneSyncSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\OneSyncSvc"},
-    @{Name = "WpnUserService"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\WpnUserService"},
-    @{Name = "CDPUserSvc"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\CDPUserSvc"}
+$registryKeys = @(
+    # Lista de servicios y sus valores de registro
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\BITS" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\BTAGService" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\bthserv" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\DiagTrack" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\HvHost" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmickvpexchange" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicguestinterface" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicshutdown" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicheartbeat" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicvmsession" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicrdv" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmictimesync" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\vmicvss" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PhoneSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Spooler" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\QWAVE" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\SysMain" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\WSearch" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\UserDataSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\UnistoreSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\UdkUserSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PimIndexMaintenanceSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\DevicesFlowUserSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PrintWorkflowUserSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\PenService" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\P9RdrService" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\MessagingService" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\BluetoothUserService" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\webthreatdefusersvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\NPSMSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\OneSyncSvc" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\WpnUserService" },
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\CDPUserSvc" }
+    @{Name = "Start"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\GpuEnergyDrv" },
+    # Miscellaneous system settings for better overall performance
+    @{Name = "PowerThrottlingOff"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" },
+    @{Name = "NetworkThrottlingIndex"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" },
+    @{Name = "SystemResponsiveness"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" },
+    @{Name = "Affinity"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "Background Only"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "Clock Rate"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "GPU Priority"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "Priority"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "Scheduling Category"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "SFIO Priority"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" },
+    @{Name = "ShowSleepOption"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" },
+    @{Name = "HibernateEnabled"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Power" },
+    @{Name = "MaintenanceDisabled"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" },
+    @{Name = "MenuShowDelay"; KeyPath = "HKCU:\Control Panel\Desktop" },
+    @{Name = "Default"; KeyPath = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" },
+    @{Name = "GlobalUserDisabled"; KeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" },
+    @{Name = "AllowNewsAndInterests"; KeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Dsh" },
+    @{Name = "AllowTelemetry"; KeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" },
+    @{Name = "WaitToKillServiceTimeout"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control" },
+    @{Name = "TaskbarAnimations"; KeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" },
+    @{Name = "ListviewAlphaSelect"; KeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" },
+    @{Name = "MaxThreads"; KeyPath = "HKLM:\SYSTEM\ControlSet001\Services\LanmanWorkstation\Parameters" },
+    @{Name = "MaxCmds"; KeyPath = "HKLM:\SYSTEM\ControlSet001\Services\LanmanWorkstation\Parameters" },
+    @{Name = "SearchOrderConfig"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" },
+    @{Name = "SynchronousUserGroupPolicy"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" },
+    @{Name = "VerboseStatus"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" },
+    @{Name = "SynchronousMachineGroupPolicy"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" },
+    @{Name = "NetworkThrottlingIndex"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" },
+    @{Name = "SystemResponsiveness"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" },
+    @{Name = "Affinity"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\DisplayPostProcessing" },
+    @{Name = "IRQ8Priority"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\PriorityControl" },
+    @{Name = "AdditionalCriticalWorkerThreads"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Executive" },
+    @{Name = "SecondLevelDataCache"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" },
+    @{Name = "EnablePrefetcher"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" },
+    @{Name = "DisableDeleteNotification"; KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" },
+    @{Name = "TCPNoDelay"; KeyPath = "HKLM:\SOFTWARE\Microsoft\MSMQ\Parameters" },
+    @{Name = "EnableFirstLogonAnimation"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" },
+    @{Name = "StartupDelayInMSec"; KeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" },
+    @{Name = "Max Cached Icons"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" },
+    @{Name = "ExtendedUIHoverTime"; KeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" },
+    @{Name = "MaxCollectionCount"; KeyPath = "HKLM:\SYSTEM\ControlSet001\Services\LanmanWorkstation\Parameters" },
+    @{Name = "Auto"; KeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug" },
+    @{Name = "AutoChkTimeOut"; KeyPath = "HKLM:\SYSTEM\ControlSet001\Control\Session Manager" }
 )
 
-# Leer y registrar los registros actuales
-foreach ($service in $services) {
-    Write-Host "Checking current value for service: $($service.Name)"
-    Show-RegistryValue -keyPath $service.KeyPath -valueName "Start" -logFilePath $logFilePath
+# Recorrer la lista de registros y mostrar sus valores
+foreach ($item in $registryKeys) {
+    Write-Host "Checking current value of the key: $($item.Name) in the "
+    Show-RegistryValue -keyPath $item.KeyPath -valueName $item.Name -logFilePath $logFilePath
+    Start-Sleep -Seconds 0.5
 }
 
-Write-Host "All services have been checked."
+Write-Host "All keypath have been checked."
+Write-Output "keyPath no encontrados: $keypathsNoFound"
+Write-Output "keys no encontrados: $keysNoFound"
+
+# Mostrar keyPath no encontrados
+if ($missingKeyPaths.Count -gt 0) {
+    "KeyPaths no encontrados:"
+    $missingKeyPaths | ForEach-Object {
+        "  $_"
+    }
+} else {
+    "No se encontraron keyPaths."
+}
+
+# Mostrar mensaje de confirmación
+Write-Output "Registros guardados en: $logFilePath"
+
+# Solicitar confirmación para cerrar la ventana de PowerShell
+Write-Output "Presiona Enter para salir..."
+$null = Read-Host
